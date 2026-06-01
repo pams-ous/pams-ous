f506cd09cfbcf71dc9ea262e05c443fa54764cbd
Francis Llego <llego.fl06@gmail.com>
2026-06-01 08:06:09 +0800

    Remove hardcoded DB password
    Remove hardcoded database password for security.

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT || 3306),
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'hillsazucena#17',
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || 'people'
 });
 
